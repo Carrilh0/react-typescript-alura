0dbac4250db44cbdbe0f07609de9a30e54466cc8
Vitor Carrilho <vitor.carrilho@hotmail.com>
2022-12-05 23:44:15 -0300

    rendering an array

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,27 +1,31 @@
 import React from 'react'
 
 function Lista() {
+  const tarefas = [{
+    name: 'React',
+    time: '02:00:00'
+  },{
+    name: 'Javascript',
+    time: '01:00:00'
+  },{
+    name: 'Typescript',
+    time: '03:00:00'
+  }]
   return (
     <aside>
       <h2>Estudos do Dia</h2>
-      <ul>
-        <li>
-          <h3>
-            React
-          </h3>
-          <span>
-            02:00:00
-          </span>
-        </li>
-        <li>
-          <h3>
-            Javascript
-          </h3>
-          <span>
-            01:00:00
-          </span>
-        </li>
-      </ul>
+        <ul>
+          {tarefas.map((item, index) => ( // () retorna JSX e {} um escopo | Uma renderização sempre precisa do key, são usadas no React para identificar quais itens na lista são alterados, atualizados ou excluídos. (Ideal que seja um ID unico)
+            <li key={index}>
+              <h3>
+                {item.name}
+              </h3>
+              <span>
+                {item.time}
+              </span>
+            </li>
+          ))}
+        </ul>
     </aside>
   )
 }
